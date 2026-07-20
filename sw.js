5eb8d86e365c3cea1ec52f3d91bc31cb9cf5764d
y058388140-png <y058388140@gmail.com>
2026-07-20 21:00:27 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dubis-v3';
-const FILES=['./dubis-app-v3.html','./manifest.webmanifest','./dubis-icon.svg','./dubis-logo.jpg'];
+const CACHE='dubis-v4';
+const FILES=['./dubis-app-v4.html','./manifest.webmanifest','./dubis-icon.svg','./dubis-logo-transparent.png','./category-covers.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./dubis-app-v3.html'))))});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./dubis-app-v4.html'))))});
