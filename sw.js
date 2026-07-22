57635631fd396c621a2a834eaf4ab8c82c13b063
y058388140-png <y058388140@gmail.com>
2026-07-23 00:14:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='dubis-v4-6';
-const FILES=['./dubis-app-v4.html','./manifest.webmanifest','./dubis-icon.svg','./dubis-logo-transparent.png','./category-covers.jpg'];
+const CACHE='dubis-v4-7';
+const FILES=['./dubis-app-v4.html','./manifest.webmanifest','./dubis-icon-d.png','./dubis-logo-transparent.png','./category-covers.jpg'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(FILES)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));return response}).catch(()=>caches.match(event.request).then(hit=>hit||caches.match('./dubis-app-v4.html'))))});
